22e62499a58e750277bb191eef5bca369d7a9f27
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-09-24 20:18:39 +0200

    [NEW]Connection database

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// index js
+require('dotenv').config()
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const mongoose      = require('mongoose');
+const mongodbRoute  = process.env.MONGO_DB_STRING;
+
+const app   = express();
+const PORT  = process.env.PORT || 3000;
+
+// Use bodyparser
+app.use(bodyParser.json());
+
+async function start(){
+    try{
+        await mongoose.connect(mongodbRoute);
+        app.listen(PORT, () => {
+            console.log(`API is listening on port ${PORT}`);
+        });
+        console.log('Conexion con Mongo Correcta');
+    } 
+    catch(error){
+        
+        console.log(`Error al conectar a la base de datos: ${error.message}`);
+    }  
+}
+
+start();
